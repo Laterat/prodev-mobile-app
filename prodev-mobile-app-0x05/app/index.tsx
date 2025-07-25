43c3631d809b9d6a0a011432a9a7cf5bef30de31
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-07-25 14:56:57 +0300

    initial

---
+++ prodev-mobile-app-0x05/app/index.tsx
@@ -0,0 +1,57 @@
+import {
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { styles } from "@/styles/_mainstyle";
+import { BACKGROUNDIMAGE, HEROLOGO } from "@/constants";
+export default function Index() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <ImageBackground
+          source={BACKGROUNDIMAGE}
+          style={styles.backgroundImageContainer}
+          resizeMode="cover"
+        >
+          <View style={styles.container}>
+            <View style={styles.logoContainer}>
+              <Image source={HEROLOGO} />
+            </View>
+
+            <View style={styles.titleContainer}>
+              <Text style={styles.titleText}>
+                Find your favorite place here
+              </Text>
+              <Text style={styles.titleSubText}>
+                The best prices for over 2{" "}
+              </Text>
+              <Text style={styles.titleSubText}>
+                million properties worldwide
+              </Text>
+            </View>
+
+            <View style={{ position: "absolute", bottom: 0, width: "100%" }}>
+              <View style={styles.buttonGroup}>
+                <TouchableOpacity style={styles.buttonPrimary}>
+                  <Text style={styles.buttonPrimaryText}>Join here</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.buttonSecondary}>
+                  <Text style={styles.buttonSecondaryText}>Sign In</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.buttonGroupSubText}>
+                <Text style={{ color: "white" }}>Continue to home</Text>
+              </View>
+            </View>
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
